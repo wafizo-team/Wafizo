f94c71b3e15777d2a20db51549827095cc686428
Roukhmi wafa <wafa.roukhmi@gmail.com>
2026-08-08 14:08:53 +0200

    fix(health): restore HealthModule in app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,15 @@
-export class AppModule {}
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { HealthModule } from './health/health.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     PrismaModule,
-    AuthModule,
-    HealthModule, //
+    HealthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
