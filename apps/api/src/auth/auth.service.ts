6d3ce3a7a9da85c7f6cc75c08d0cb8354984ec80
Roukhmi wafa <wafa.roukhmi@gmail.com>
2026-09-03 01:44:52 +0200

    fix(auth): fix unsafe assignment in auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@nestjs/common';
 export class AuthService {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   googleLogin(req: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     const user = req?.user;
     if (!user) {
       return { message: 'No user from Google' };
     }
     return {
       message: 'User info from Google',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       user,
     };
   }
