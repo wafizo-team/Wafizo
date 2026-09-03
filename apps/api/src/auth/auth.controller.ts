20ed5075f5f2ff37d8d0278cb717e7a8661247e9
Roukhmi wafa <wafa.roukhmi@gmail.com>
2026-09-03 21:17:08 +0200

    feat(auth): update googleAuthRedirect to generate JWT and redirect to frontend

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import type { Request } from 'express';
+import type { Request, Response } from 'express';
 import { Public } from './decorators/public.decorator';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Public()
   @Get('google')
   @UseGuards(AuthGuard('google'))
@@ -15,11 +18,8 @@ export class AuthController {
   @Public()
   @Get('google/callback')
   @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req: Request) {
-    const user = (req as Request & { user: unknown }).user;
-    return {
-      message: 'Connexion Google réussie !',
-      user,
-    };
+  async googleAuthRedirect(@Req() req: Request, @Res() res: Response) {
+    const result = await this.authService.googleLogin(req);
+    return res.redirect(`https://app.wafizo.fr?accessToken=${result.accessToken}`);
   }
 }
