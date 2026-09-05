e7dbddf790c65dfe6330c98a793193f93d599e12
Lassana Sylla <lassanasylla2070@gmail.com>
2026-09-06 01:56:33 +0200

    fix(api): register BusinessModule in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module'; // <-- Correction ici (.module au lieu de .service)
+import { BusinessModule } from './business/business.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PrismaModule } from './prisma/prisma.module'; // <-- Correction ici (.m
     }),
     AuthModule,
     PrismaModule,
+    BusinessModule,
   ],
   controllers: [AppController],
   providers: [AppService],
