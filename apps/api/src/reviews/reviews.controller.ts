7932a7e0b8aa3615ee13097fa28269b3db357fbb
Roukhmi wafa <wafa.roukhmi@gmail.com>
2026-09-13 15:55:30 +0200

    fix(api): pass query DTO to reviewsService.findAll

--- apps/api/src/reviews/reviews.controller.ts
+++ apps/api/src/reviews/reviews.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
+import { GetReviewsQueryDto } from './dto/get-reviews-query.dto';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
 
   @Get()
-  getReviews(@Query() _query: Record<string, unknown>) {
-    return this.reviewsService.findAll();
+  getReviews(@Query() query: GetReviewsQueryDto) {
+    return this.reviewsService.findAll(query);
   }
 }
